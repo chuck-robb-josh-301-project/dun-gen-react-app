c1b358ef9d238558e4911ca4005c1a8d7df3d680
Charles Altopiedi <Chuckalto@gmail.com>
2022-02-01 19:09:53 -0500

    imported bootstrap css, app.css removed modal handling

--- src/App.js
+++ src/App.js
@@ -2,28 +2,20 @@ import { withAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 import LoginButton from './LoginButton.js';
 import LogoutButton from './LogoutButton.js';
-import HeroSelector from './heroSelect.js';
+import HeroSelector from './HeroSelector.js';
 import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false
+      // showModal: false
     }
   }
 
-  handleOpen = () => {
-    this.setState({
-      showModal: true
-    })
-  }
-
-  handleClose = () => {
-    this.setState({
-      showModal: false
-    })
-  }
 
   render() {
     return (
@@ -33,15 +25,11 @@ class App extends React.Component {
         {this.props.auth0.isAuthenticated ? <h2>Good Morning Chuck!</h2> : <h2>Please Log In!</h2>}
         {this.props.auth0.isAuthenticated ?
         <Container>
-          <HeroSelector 
-          show={this.state.showModal}
-          onHide={this.state.handleclose}
-          centered
-          size="xl"
-          />
+          <HeroSelector/>
         </Container> 
         : <h2></h2>
-  }
+        } 
+        
       </>
     )
   }
